db48e118c2ef7e9d5a8f893ed4a34b1ddcd3cd79
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2023-11-29 18:07:23 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        //Dependecy injection 
+        ICarDal _carService;
+        public CarManager(ICarDal carService)
+        {
+            _carService = carService;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carService.GetAll();
+        }
+
+        public Car GetByIdCar(int id)
+        {
+            return _carService.Get(c=>c.id==id);
+        }
+    }
+}
---
+++ ReCapProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ReCapProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager cars = new CarManager(new EfCarDal());
+            foreach (var c in cars.GetAll())
+            {
+                Console.WriteLine(c.Description );
+            }
+        }
+    }
+}
